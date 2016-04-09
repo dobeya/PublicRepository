416f9fd7d04f8c6a30449430b5d40a0df68e6f0a
dobeya <1163142850@qq.com>
2016-04-09 20:33:15 +0800

    测试返回JSON

--- Registration/src/com/registration/controller/ApplyController.java
+++ Registration/src/com/registration/controller/ApplyController.java
@@ -1,10 +1,16 @@
 package com.registration.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.alibaba.fastjson.JSON;
+import com.registration.po.User;
 import com.registration.service.TestService;
+import com.registration.tools.ResponseUtils;
 
 @Controller
 @RequestMapping("/application")
@@ -12,7 +18,12 @@ public class ApplyController {
 	@Autowired
 	private TestService testService;
 	@RequestMapping("/submit")
-	public void submitApplication(){
-		testService.showMsg();
+	public void submitApplication(HttpServletRequest request,HttpServletResponse response){
+		User user = new User();
+		user.setAddress("µÿ÷∑");
+		user.setEmail("email");
+		user.setId("123456");
+		user.setCity("≥« –");
+		ResponseUtils.writeJson(response, JSON.toJSONString(user));
 	}
 }
