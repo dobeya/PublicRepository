0a0b29153ea48bb90f0b9cb41a650cd5a422d2dc
dobeya <1163142850@qq.com>
2016-04-09 18:17:53 +0800

    更新

--- Registration/src/com/registration/controller/ApplyController.java
+++ Registration/src/com/registration/controller/ApplyController.java
@@ -1,10 +1,18 @@
 package com.registration.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-@RequestMapping("/apply")
-public class ApplyController {
+import com.registration.service.TestService;
 
+@Controller
+@RequestMapping("/application")
+public class ApplyController {
+	@Autowired
+	private TestService testService;
+	@RequestMapping("/submit")
+	public void submitApplication(){
+		testService.showMsg();
+	}
 }
