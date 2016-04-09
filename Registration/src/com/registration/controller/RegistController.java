12c2db9d553b5713a8ac096aafcd5dfc8d82267a
dobeya <1163142850@qq.com>
2016-04-09 18:18:17 +0800

    更新

--- Registration/src/com/registration/controller/RegistController.java
+++ Registration/src/com/registration/controller/RegistController.java
@@ -1,24 +1,16 @@
 package com.registration.controller;
 
-import org.mybatis.spring.SqlSessionTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.registration.service.TestService;
-
 @Controller
-@RequestMapping("/regist")
+@RequestMapping("/registration")
 public class RegistController {
-	@Autowired
-	private TestService test;
-	@Autowired
-    private SqlSessionTemplate sqlSessionTemplate;
-	@RequestMapping("/test")
-	public void showMsg(){
-		System.out.println("\nHEHEHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHello\n");
-		test.showMsg();
-		System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-		sqlSessionTemplate.selectOne("commonDAO.getInt");
+	@RequestMapping("/regist")
+	public void regist(HttpServletRequest req,HttpServletResponse res){
+		
 	}
 }
